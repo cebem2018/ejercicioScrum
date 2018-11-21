27e413361e737663609771ffa9b7fdfd00af35b1
EnforcerZhukov <davidl.eco@gmail.com>
2018-11-21 09:44:43 +0100

    Add documentacion y doctest

--- limpiar_texto.py
+++ limpiar_texto.py
@@ -1,5 +1,14 @@
 
 def limpiar(texto: str) -> str:
+    """El texto que recibe se devuelve limpio, eliminando espacios dobles y los espacios que haya
+    tanto al principio como al final.
+    :param texto: Texto de entrada
+    :type texto: str
+    :return: Texto de entrada limpio (eliminando 
+    :rtype: str
+    >>> limpiar("estoy escribiendo desde  móvil y meto un espacio al   final y algunos   por enmedio ")
+    'estoy escribiendo desde móvil y meto un espacio al final y algunos por enmedio'
+    """
     texto = texto.strip()
     return " ".join(texto.split())
 
