a1aadd06d91852cc9b535f0f33fcd11b04020602
Enriquecerviro <enrique.cervirodam@gmail.com>
2018-11-21 09:53:32 +0100

    falta documentacion

---
+++ 20CrearyEscribir.py
@@ -0,0 +1,14 @@
+# Lista con las lineas a escribir en el fichero
+lineas = ['Uno', 'Dos', 'Tres', 'Cuatro', 'Cinco', 'Seis', '...']
+
+dirFichero = './fichero_escribir.txt'
+#opción 'w' (si existe el fichero borra su contenido y si no existe lo crea)
+#opción 'a+' que se posiciona al final del fichero sin borrar su contenido y
+#  en este caso empieza a escribir contenido a partir de esa posición.
+fichero = open(dirFichero, 'w')
+for l in lineas:
+    fichero.write(l + '\n')
+fichero.close()
+
+#hacemos test con el siguiente codigo
+#python -m doctest -v nombre.py
