493f694b22dbe47ce1cedcba897a4fa28872b746
BeeCtown <boule@boulemusic.com>
2018-11-21 09:42:31 +0100

    Add test

--- bee_swag.py
+++ bee_swag.py
@@ -1,6 +1,11 @@
-string = "hEY YO hOMIE"
+
 
 def texto_con_swag(string):
+    """Invierte el texto recibido entre mayusculas/minusculas
+    >>> texto_con_swag("hEY YO hOMIE")
+    'Hey yo Homie'
+    """
     return string.swapcase()
-
-print(texto_con_swag(string))
+if __name__ == "__main__":
+    string = "hEY YO hOMIE"
+    print(texto_con_swag(string))
